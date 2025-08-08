93f57745700076d380e65d5825e9f84c01a080f1
Semere Tereffe <se.semere.tereffe@gmail.com>
2025-08-08 23:54:39 +0300

    x

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-const { resolve } = await import("node:path");
+const { resolve } = await import("path");
 // import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
