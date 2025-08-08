df107a787434ac05950ecdea36457ef39b77cf35
Semere Tereffe <se.semere.tereffe@gmail.com>
2025-08-08 23:58:54 +0300

    x

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+import { fileURLToPath } from "url";
+
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-const { resolve } = await import("path");
+import path from "path";
+// const { resolve } = await import("path");
 // import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define resolve helper
+const resolve = (...segments: string[]) => path.resolve(__dirname, ...segments);
+
 export default defineConfig({
 	plugins: [
 		TanStackRouterVite({ autoCodeSplitting: true }),
@@ -19,7 +28,7 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			"@": "./src",
+			"@": resolve("./src"),
 		},
 	},
 });
